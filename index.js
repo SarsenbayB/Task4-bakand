3631569cb87d2865fa0019256a6a82350d3b60d4
Bakbergen Sarsenbay <bakbergen.sarsenbay@mail.ru>
2024-09-28 11:33:11 +0500

    cors2

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: 'https://task4-one-mu.vercel.app'
+    origin: ['https://task4-one-mu.vercel.app', 'https://task4-files.vercel.app']
 }));
 
 app.post("/auth/login", loginValidation, UserController.login);
